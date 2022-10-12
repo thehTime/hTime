b3015fe710966c8155856936d8e13b396bb7c04d
Alessandro Bellini <alessandro@ilmente.it>
2022-10-12 21:59:30 +0200

    add local clock

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
+import { HTime as HTimeType, createHTime } from './Core/HTime';
 import { GlobalHour as GlobalHourType } from './Core/GlobalHour';
 import { isIsoUTCDateString, isHTimeDateString, parseToUTCDate } from './Core/Parser';
-import { HTime as HTimeType, createHTime } from './Core/HTime';
+import { createLocalClock } from './Util/Clock';
 
-/**
- * Types
- */
+// types
 export type HTime = HTimeType;
 export type GlobalHour = GlobalHourType;
 
-/**
- * Api
- */
+// api
 export {
+  // core
   createHTime,
   isIsoUTCDateString,
   isHTimeDateString,
   parseToUTCDate,
+
+  // util
+  createLocalClock,
 }

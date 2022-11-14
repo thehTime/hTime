fc4adb4a2598a7e8bc5758c9e40b334c738b0702
Alessandro Bellini <alessandro@ilmente.it>
2022-11-14 22:03:30 +0100

    add tests for past and future

--- test/Util/Comparison.test.js
+++ test/Util/Comparison.test.js
@@ -1,4 +1,4 @@
-import { isBefore, isAfter, isSame } from '@/Util/Comparison';
+import { isBefore, isPast, isAfter, isFuture, isSame } from '@/Util/Comparison';
 import { createHTime } from '@/Core/HTime';
 
 let before, after, now;
@@ -16,23 +16,37 @@ beforeEach(() => {
 })
 
 describe('isBefore()', () => {
-  test('return new htime with added milliseconds', () => {
+  test('return ...', () => {
     expect(isBefore(before, after)).toBe(true);
     expect(isBefore(after, before)).toBe(false);
     expect(isBefore(now, now)).toBe(false);
   });
 });
 
+describe('isPast()', () => {
+  test('return ...', () => {
+    expect(isPast(before)).toBe(true);
+    expect(isPast(after)).toBe(false);
+  });
+});
+
 describe('isAfter()', () => {
-  test('return new htime with added milliseconds', () => {
+  test('return ...', () => {
     expect(isAfter(after, before)).toBe(true);
     expect(isAfter(before, after)).toBe(false);
     expect(isAfter(now, now)).toBe(false);
   });
 });
 
+describe('isFuture()', () => {
+  test('return ...', () => {
+    expect(isFuture(after)).toBe(true);
+    expect(isFuture(before)).toBe(false);
+  });
+});
+
 describe('isSame()', () => {
-  test('return new htime with added milliseconds', () => {
+  test('return ...', () => {
     expect(isSame(after, before)).toBe(false);
     expect(isSame(before, after)).toBe(false);
     expect(isSame(now, now)).toBe(true);

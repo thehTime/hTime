d354ace27a9a5760a5455230a0d3385f36f1ae23
Alessandro Bellini <alessandro@ilmente.it>
2022-11-16 11:13:01 +0100

    remove module inconvenience

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,15 +1,21 @@
 import { defineConfig } from 'rollup';
 import typescript from '@rollup/plugin-typescript';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
   input: 'src/Api.ts',
-  external: ['date-fns', 'date-fns-tz'],
-  plugins: [typescript()],
+
   output: {
     dir: 'dist',
     format: 'cjs',
     sourcemap: true,
   },
+
+  external: [
+    'date-fns',
+    'date-fns-tz'
+  ],
+
+  plugins: [
+    typescript()
+  ],
 });

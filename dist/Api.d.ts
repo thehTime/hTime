db887f70db92cedd38538b66509164aefb2da861
Alessandro Bellini <alessandro@ilmente.it>
2022-12-20 21:38:42 +0100

    expose convertion to api

--- dist/Api.d.ts
+++ dist/Api.d.ts
@@ -1,10 +1,11 @@
 import * as Constant from './Core/Constant';
 import { HTime as HTimeType, createHTime } from './Core/HTime';
 import { GlobalHour as GlobalHourType } from './Core/GlobalHour';
+import { fromDaysToMilliseconds, fromMillisecondsToMinutes, fromMinutesToHours, fromMinutesToMilliseconds, fromWeeksToMilliseconds } from './Core/Conversion';
 import { isUtcIsoDateString, isHTimeDateString, createDateString, breakdownDateString } from './Core/Parse';
 import { createClock } from './Util/Clock';
 import { isBefore, isPast, isAfter, isFuture, isSame } from './Util/Comparison';
 import { addMilliseconds, addSeconds, addMinutes, addHours, addDays, addWeeks, subMilliseconds, subSeconds, subMinutes, subHours, subDays, subWeeks } from './Util/Operation';
 export declare type HTime = HTimeType;
 export declare type GlobalHour = GlobalHourType;
-export { Constant, createHTime, isUtcIsoDateString, isHTimeDateString, createDateString, breakdownDateString, createClock, addMilliseconds, addSeconds, addMinutes, addHours, addDays, addWeeks, subMilliseconds, subSeconds, subMinutes, subHours, subDays, subWeeks, isBefore, isPast, isAfter, isFuture, isSame, };
+export { Constant, fromMinutesToMilliseconds, fromDaysToMilliseconds, fromWeeksToMilliseconds, fromMillisecondsToMinutes, fromMinutesToHours, createHTime, isUtcIsoDateString, isHTimeDateString, createDateString, breakdownDateString, createClock, addMilliseconds, addSeconds, addMinutes, addHours, addDays, addWeeks, subMilliseconds, subSeconds, subMinutes, subHours, subDays, subWeeks, isBefore, isPast, isAfter, isFuture, isSame, };
